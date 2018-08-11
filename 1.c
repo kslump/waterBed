a96f9cf1d5a1cd37f40c48e6fab3e9c5ad05500c
Dr. slump <6176382+kslump@users.noreply.github.com>
2018-08-11 12:34:44 +0800

    ooo

--- 1.c
+++ 1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 是的水电费速度速度
+
 int main() {
+   uint8_t ssf;
+
    printf("hello,world");
    printf("hello,world2\n");
 
    return 0;
 }
-
-
-
-
 
 
 
